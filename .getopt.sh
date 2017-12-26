bb927f4e90d5f8bba769b0beca9275bda2a6e8e7
Re4son <Re4son@users.noreply.github.com>
2017-12-27 09:23:36 +1100

    Release Pocket-Kali 1.0 live build

---
+++ .getopt.sh
@@ -0,0 +1,11 @@
+# Supported options are:
+# -d | --distribution <distro>
+# -p | --proposed-updates
+# -a | --arch <architecture>
+#      --subdir <directory-name>
+#      --version <version>
+#      --variant <variant>
+# -v | --verbose
+# -s | --salt
+BUILD_OPTS_SHORT="d:pa:vs"
+BUILD_OPTS_LONG="distribution:,proposed-updates,arch:,subdir:,version:,variant:,verbose,salt"
